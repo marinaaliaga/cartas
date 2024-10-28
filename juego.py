6e6fd69c5e0468f0af67ec6953073134e71cf6d9
marinaaliaga <al.marinaaliaga@iesramonllull.net>
2024-10-28 11:56:53 +0100

    third commit

--- juego.py
+++ juego.py
@@ -4,9 +4,30 @@ from mano import Mano
 class Juego:
     def __init__(self):
         self.baraja= Baraja()
-        self.baraja.barajar()
+        self.baraja.Baraja()
     
     def jugar(self):
         manoJugador=Mano()
         manoJugador.añadirCarta(self.baraja.sacarCarta())
-        print(f"Tu mano es {manoJugador.cartas}")
+        print(f"Tu mano es{manoJugador.cartas}")
+        print(f"Total:{manoJugador.valor}")
+        while manoJugador.valor <21:
+            accion= input("¿Quieres pedir carta o pasar?").lower()
+            if accion == "pedir":
+                manoJugador.añadirCarta(self.baraja.sacarCarta())
+                print(f"Tu mano es {manoJugador.cartas()}")
+                print(f"valor {manoJugador.valor}")
+            else:
+                print(f"Tu puntuacion final es {manoJugador.valor}")
+                return
+        if manoJugador.valor ==21:
+            print ("ENHORABUENA")
+        elif manoJugador. valor <21:
+            print("No has llrgado a 21")
+        else:
+            print("Te has pasado")
+
+if __name__ == '__main__':
+    print("JUEGO DE 21")
+    juego= Juego()
+    juego.jugar()
