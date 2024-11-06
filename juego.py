36cf53d7bf20521eb648d31fe753fd30a07481a4
marinaaliaga <al.marinaaliaga@iesramonllull.net>
2024-11-06 09:50:49 +0100

    cuarto commit

--- juego.py
+++ juego.py
@@ -1,5 +1,6 @@
 from baraja import Baraja
 from mano import Mano
+from carta import Carta
 
 class Juego:
     def __init__(self):
@@ -26,8 +27,36 @@ class Juego:
             print("No has llegado a 21")
         else:
             print("Te has pasado")
+            
+    def __init__(self):
+        self.jugador1 = Mano()
+        self.jugador2 = Mano()
+    
+    def repartirCartas(self,cartas_j1, cartas_j2):
+        for carta in cartas_j1:
+            self.jugador1.añadirCarta(carta)
+
+        for carta in cartas_j2:
+            self.jugador2.añadirCarta(carta) 
+
+    def mostrarManos(self):
+        print("Mano del jugador 1:")
+        self.jugador1.mostrarMano()
+        print("Valor total:", self.jugador1.valor)
+
+        print("Mano del jugador 2:")
+        self.jugador2.mostrarMano()
+        print("Valor total:", self.jugador2.valor)
+
+juego =Juego()
+
+cartas_j1 = [Carta("As",11), Carta("5",5)]
+cartas_j2 = [Carta("Rey",10), Carta("3",3)]
+
+juego.repartirCartas(cartas_j1,cartas_j2)
+juego.mostrarManos()
 
 if __name__ == '__main__':
     print("JUEGO DE 21")
-    juego= Juego()
+    juego = Juego()
     juego.jugar()
--- mano.py
+++ mano.py
@@ -18,3 +18,32 @@ class Mano:
     def mostrarMano(self):
         for carta in self.cartas:
             print(carta)
+
+class Juego:
+    def __init__(self):
+        self.jugador1 = Mano()
+        self.jugador2 = Mano()
+    
+    def repartirCartas(self,cartas_j1, cartas_j2):
+        for carta in cartas_j1:
+            self.jugador1.añadirCarta(carta)
+
+        for carta in cartas_j2:
+            self.jugador2.añadirCarta(carta) 
+
+    def mostrarManos(self):
+        print("Mano del jugador 1:")
+        self.jugador1.mostrarMano()
+        print("Valor total:", self.jugador1.valor)
+
+        print("Mano del jugador 2:")
+        self.jugador2.mostrarMano()
+        print("Valor total:", self.jugador2.valor)
+
+juego =Juego()
+
+cartas_j1 = [Carta("As",11), Carta("5",5)]
+cartas_j2 = [Carta("Rey",10), Carta("3",3)]
+
+juego.repartirCartas(cartas_j1,cartas_j2)
+juego.mostrarManos()
